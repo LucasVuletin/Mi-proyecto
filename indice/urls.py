232e4c1b1e4adacf6bf587be0deccec2b4351566
Lucas Vuletin <lucas.vuletin@remasa.com>
2022-04-09 00:45:21 -0300

    1 hora de int2

--- indice/urls.py
+++ indice/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import inicio, otra_vista, numero_random, numero_del_usuario, mi_plantilla   
   
 urlpatterns = [   
-    path('', inicio),
-    path('otra-vista', otra_vista),
-    path('numero-random/', numero_random),
-    path('dame-numero/<int:numero>', numero_del_usuario), #A esto me referia en views, ademas agrego INT para que sepa que es num
-    path('mi-plantilla', mi_plantilla), 
+    path('', inicio, name="inicio"),
+    path('otra-vista', otra_vista, name="otra_vista"),
+    path('numero-random/', numero_random, name="numero_random"),
+    path('dame-numero/<int:numero>', numero_del_usuario, name="numero_del_usuario"), #A esto me referia en views, ademas agrego INT para que sepa que es num
+    path('mi-plantilla', mi_plantilla, name="mi_plantilla"), 
 ]
