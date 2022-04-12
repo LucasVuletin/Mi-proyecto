e01fbb772e86cd0ca54592af2729f6a6c1a68518
Lucas Vuletin <lucas.vuletin@remasa.com>
2022-04-12 00:35:19 -0300

    terminado int2

--- clase/admin.py
+++ clase/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from clase.models import curso
+
 # Register your models here.
+
+from .models import curso, profesor, entregable, estudiante
+
+admin.site.register(curso)
+admin.site.register(profesor)
+admin.site.register(entregable)
+admin.site.register(estudiante)
