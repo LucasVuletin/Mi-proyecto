4cd464677b8c32617ed552061e496da5abf1d3e2
Lucas Vuletin <lucas.vuletin@remasa.com>
2022-04-16 03:26:59 -0300

    termine av1

--- clase/urls.py
+++ clase/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
-from .views import formulario_curso, nuevo_curso, busqueda_curso
+# from .views import formulario_curso, nuevo_curso, busqueda_curso, crear_estudiante, estudiante, borrar_estudiante, actualizar_estudiante, listado_estudiantes
+from . import views
+
 urlpatterns = [
-    path('nuevo/', nuevo_curso, name='nuevo_curso'),
-    path('formulario_curso', formulario_curso, name='formulario_curso'),
-    path('busqueda_curso', busqueda_curso, name='busqueda_curso')
+    path('nuevo/', views.nuevo_curso, name='nuevo_curso'),
+    path('formulario_curso', views.formulario_curso, name='formulario_curso'),
+    path('busqueda_curso', views.busqueda_curso, name='busqueda_curso'),
+
+    # path('estudiante/', estudiante, name='estudiante'),
+    path('estudiante/listado_estudiantes', views.listado_estudiantes, name='listado_estudiantes'),
+    path('estudiante/crear_estudiante', views.crear_estudiante, name='crear_estudiante'),
+    path('estudiante/borrar/<int:id>', views.borrar_estudiante, name='borrar_estudiante'),
+    path('estudiante/actualizar/<int:id>', views.actualizar_estudiante, name='actualizar_estudiante'),
+
+    path('profesores', views.ProfesorLista.as_view(), name="profesor_listado"),
+    path('profesores/<int:pk>', views.ProfesorDetalle.as_view(), name="profesor_detalle"),
+    path('profesores/<int:pk>/editar', views.ProfesorEditar.as_view(), name="profesor_editar"),
+    path('profesores/<int:pk>/borrar', views.ProfesorBorrar.as_view(), name="profesor_borrar")
 ]
--- clase/views.py
+++ clase/views.py
@@ -1,7 +1,10 @@
+from dataclasses import fields
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView
 from django.http import HttpResponse
-from django.shortcuts import render
-from clase.models import curso 
-from clase.forms import BusquedaCurso, CursoFormulario
+from django.shortcuts import render, redirect
+from clase.models import curso, estudiante, profesor 
+from clase.forms import BusquedaCurso, CursoFormulario, EstudianteFormulario
 import random
 # Create your views here.
 
@@ -45,3 +48,83 @@ def busqueda_curso(request):
 
     buscador = BusquedaCurso()
     return render(request, "busqueda_curso.html", {'buscador': buscador, 'curso_buscado': curso_buscado}, {'dato': dato})
+
+#CRUD Basico
+
+def listado_estudiantes(request):
+    listado_estudiantes = estudiante.objects.all()
+    return render(
+        request, "listado_estudiantes.html",
+        {'listado_estudiantes': listado_estudiantes}
+    )    
+
+def crear_estudiante(request):
+    if request.method == 'POST':
+        formulario = EstudianteFormulario(request.POST)
+
+        if formulario.is_valid():
+            data = formulario.cleaned_data
+            nuevo_estudiante = estudiante(nombre=data['nombre'], apellido=data['apellido'], email=data['email'])
+            nuevo_estudiante.save()
+            # return render(request, 'listado_estudiantes.html', {})
+            return redirect('listado_estudiantes')
+
+    formulario = EstudianteFormulario()
+    return render(request, 'crear_estudiante.html', {'formulario': formulario})
+
+def actualizar_estudiante(request, id):
+    estudianteid = estudiante.objects.get(id=id)
+    
+
+    if request.method == 'POST':
+        formulario = EstudianteFormulario(request.POST)
+        if formulario.is_valid():
+            data = formulario.cleaned_data
+            estudiante.nombre = data['nombre'],
+            estudiante.apellido = data['apellido'],
+            estudiante.email = data['email']
+            estudiante.save()
+            # return render(request, 'listado_estudiantes.html', {})
+            return redirect('listado_estudiantes')
+
+    formulario = EstudianteFormulario(
+        initial={
+            'nombre': estudiante.nombre,
+            'apellido': estudiante.apellido,
+            'email': estudiante.email
+        }
+    )
+    return render(request, 'actualizar_estudiante.html', 
+    {'formulario': formulario, 'estudiante': estudianteid})
+
+def borrar_estudiante(request, id):    
+    estudianteid = estudiante.objects.get(id=id)
+    estudianteid.delete()
+    return redirect('listado_estudiantes')
+
+#CRUD Clases basadas en listas    
+
+# class profesor(models.Model):
+#     nombre = models.CharField(max_length=20)
+#     apellido = models.CharField(max_length=30)
+#     email = models.EmailField()
+#     profesion = models.CharField(max_length=30)
+
+class ProfesorLista(ListView):
+    model = profesor
+    template_name = 'profesor_list.html'
+
+class ProfesorDetalle(DetailView):
+    model = profesor
+    template_name = 'profesor_datos.html'
+
+class ProfesorEditar(UpdateView):
+    model = profesor
+    success_url = 'profesor_editar.html'
+    template_name = 'profesor_editar.html'
+    fields = ['nombre', 'apellido', 'email', 'profesion']
+
+class ProfesorBorrar(DeleteView):    
+    model = profesor
+    success_url = 'profesor_borrar.html'  
+    template_name = 'profesor_borrar.html' 
